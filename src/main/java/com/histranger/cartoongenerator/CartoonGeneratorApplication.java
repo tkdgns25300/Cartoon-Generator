baac64733d55652f1670e314bd80505e3edc6aa4
이상훈 <tkdgns25300@gmail.com>
2025-04-04 15:54:10 +0900

    Initial Java/Spring Setting

---
+++ src/main/java/com/histranger/cartoongenerator/CartoonGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.histranger.cartoongenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoonGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/histranger/cartoongenerator/CartoonGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.histranger.cartoongenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoonGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
